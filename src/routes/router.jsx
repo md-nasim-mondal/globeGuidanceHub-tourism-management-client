1ebb05072755debd06b907f991a76f12e28c1066
Nasim <nasim.programmer24.com>
2024-04-27 09:45:20 +0600

    completed basic setup for this projects

---
+++ src/routes/router.jsx
@@ -0,0 +1,21 @@
+import { createBrowserRouter } from "react-router-dom";
+import Root from "../layouts/Root";
+import ErrorPage from "./../pages/ErrorPage/ErrorPage";
+import Home from "../pages/Home/Home";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Root/>,
+        errorElement: <ErrorPage/>,
+        children: [
+            {
+                path: '/',
+                element: <Home/>
+            },
+            
+        ]
+    }
+]);
+
+export default router;
