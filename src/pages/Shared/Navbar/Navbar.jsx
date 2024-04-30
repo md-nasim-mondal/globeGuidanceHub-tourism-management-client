b53344b64a8565b522b2e719f60a5569f3005ca8
Nasim <nasim.programmer24.com>
2024-04-30 20:59:40 +0600

    made details page for tourists spot

--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -75,7 +75,7 @@ const Navbar = () => {
           All Tourists Spot
         </NavLink>
       </li>
-      {user ?  (
+      {user ? (
         <>
           <li>
             <NavLink
@@ -102,7 +102,7 @@ const Navbar = () => {
             </NavLink>
           </li>
           <li className={`${user ? "2xl:hidden" : "lg:hidden"}`}>
-          <button
+            <button
               onClick={() => {
                 setOpen(false);
                 handleSignOut();
@@ -112,8 +112,10 @@ const Navbar = () => {
             </button>
           </li>
         </>
-      ): (<>
-        <NavLink
+      ) : (
+        <>
+          <li className={`${user ? "2xl:hidden" : "lg:hidden"}`} >
+            <NavLink
               onClick={() => setOpen(false)}
               className={({ isActive }) =>
                 !isActive
@@ -123,7 +125,9 @@ const Navbar = () => {
               to={"/signIn"}>
               <button>Sign In</button>
             </NavLink>
-      </>)}
+          </li>
+        </>
+      )}
     </>
   );
   return (
@@ -138,9 +142,9 @@ const Navbar = () => {
             ${user ? "2xl:hidden" : "lg:hidden"}
                             `}>
             {open ? (
-              <IoClose className="text-2xl text-primary-content" />
+              <IoClose className="text-2xl text-black dark:text-primary-content" />
             ) : (
-              <IoMenu className="text-2xl text-primary-content" />
+              <IoMenu className="text-2xl text-black dark:text-primary-content" />
             )}
           </div>
           <ul
@@ -193,18 +197,21 @@ const Navbar = () => {
               </div>
             </div>
             <div className={`hidden ${user ? "2xl:flex" : "lg:flex"}`}>
-            <button
-              onClick={() => {
-                setOpen(false);
-                handleSignOut();
-              }}
-              className="btn btn-sm md:btn-md ml-0.5 md:ml-4 text-lg font-semibold btn-outline bg-none border-solid border border-[#23BE0A] rounded-lg text-[#23BE0A]">
-              SignOut
-            </button>
+              <button
+                onClick={() => {
+                  setOpen(false);
+                  handleSignOut();
+                }}
+                className="btn btn-sm md:btn-md ml-0.5 md:ml-4 text-lg font-semibold btn-outline bg-none border-solid border border-[#23BE0A] rounded-lg text-[#23BE0A]">
+                SignOut
+              </button>
             </div>
           </div>
         ) : (
-          <div className= {`hidden ${user ? "2xl:flex" : "lg:flex"} flex-col md:flex-row gap-2`} >
+          <div
+            className={`hidden ${
+              user ? "2xl:flex" : "lg:flex"
+            } flex-col md:flex-row gap-2`}>
             <NavLink
               onClick={() => setOpen(false)}
               className={({ isActive }) =>
@@ -228,41 +235,41 @@ const Navbar = () => {
           </div>
         )}
         <div className="ml-2">
-        <label className="cursor-pointer grid place-items-center ">
-          <input
-            onChange={handleToggle}
-            type="checkbox"
-            checked={theme === "dark"}
-            className="toggle theme-controller bg-base-content row-start-1 col-start-1 col-span-2"
-          />
-          <svg
-            className="col-start-1 row-start-1 stroke-base-100 fill-base-100"
-            xmlns="http://www.w3.org/2000/svg"
-            width="14"
-            height="14"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round">
-            <circle cx="12" cy="12" r="5" />
-            <path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4" />
-          </svg>
-          <svg
-            className="col-start-2 row-start-1 stroke-base-100 fill-base-100"
-            xmlns="http://www.w3.org/2000/svg"
-            width="14"
-            height="14"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round">
-            <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
-          </svg>
-        </label>
+          <label className="cursor-pointer grid place-items-center ">
+            <input
+              onChange={handleToggle}
+              type="checkbox"
+              checked={theme === "dark"}
+              className="toggle theme-controller bg-base-content row-start-1 col-start-1 col-span-2"
+            />
+            <svg
+              className="col-start-1 row-start-1 stroke-base-100 fill-base-100"
+              xmlns="http://www.w3.org/2000/svg"
+              width="14"
+              height="14"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round">
+              <circle cx="12" cy="12" r="5" />
+              <path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4" />
+            </svg>
+            <svg
+              className="col-start-2 row-start-1 stroke-base-100 fill-base-100"
+              xmlns="http://www.w3.org/2000/svg"
+              width="14"
+              height="14"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round">
+              <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
+            </svg>
+          </label>
         </div>
       </div>
       <Tooltip id="my-tooltip" place="left" />
